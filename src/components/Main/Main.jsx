233e23370064cb82a1e0882f246022447969c7e5
brianwu <fslikeyou1033@yahoo.com>
2024-11-19 14:29:53 -0500

    add components

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,11 @@
+import "./Main.scss";
+
+function Main(prop) {
+    return (
+        <>
+            
+        </>
+    )
+};
+
+export default Main;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,12 @@
+import Hero from "../../components/Hero/Hero";
+import Main from "../../components/Main/Main";
 import "./Home.scss";
 
 function Home(prop) {
     return (
         <>
-            
+            <Hero />
+            <Main />
         </>
     )
 };
