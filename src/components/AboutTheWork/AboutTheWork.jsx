a8344c7b89e6621e032ea47162c1bd92492f130b
brianwu <fslikeyou1033@yahoo.com>
2024-12-02 17:23:22 -0500

    add img and about

---
+++ src/components/AboutTheWork/AboutTheWork.jsx
@@ -0,0 +1,41 @@
+import { div } from "motion/react-client";
+import { useState } from "react";
+import "./AboutTheWork.scss";
+
+function AboutTheWork({ description, provenance }) {
+    const [currentTab, setCurrentTab] = useState("about");
+    
+
+
+
+    return (
+        <div className="lot__about">
+            <div className="lot__tab-container">
+                <div 
+                    className={`lot__tab ${currentTab === "about" ? "lot__tab--select":""}`}
+                    onClick={() => setCurrentTab("about")}
+                >
+                    <span>About the work</span>
+                </div>
+                <div 
+                    className={`lot__tab ${currentTab === "provenance" ? "lot__tab--select":""}`}
+                    onClick={() => setCurrentTab("provenance")}
+                >
+                    <span>Provenance</span>
+                </div>
+            </div>
+            { currentTab === "about" &&
+                <div className="lot__detail-text">
+                    {description}
+                </div>
+            }
+            { currentTab === "provenance" &&
+                <div className="lot__detail-text">
+                    <p>{provenance}</p>
+                </div>
+            }
+        </div>
+    )
+};
+
+export default AboutTheWork;
--- src/pages/BidPage/BidPage.jsx
+++ src/pages/BidPage/BidPage.jsx
@@ -1,10 +1,25 @@
+import { useParams } from "react-router-dom";
+import testImg1 from "../../assets/896.jpg";
+import AboutTheWork from "../../components/AboutTheWork/AboutTheWork";
+import ArtworkImage from "../../components/ArtworkImage/ArtworkImage";
 import "./BidPage.scss";
 
-function BidPage(prop) {
+function BidPage() {
+    const { auctionId } = useParams();
+    
+    // api get auction by id
+    // api get artwork by artwork_id
+    
     return (
-        <>
-            
-        </>
+        <div className="lot">
+            <div className="lot__left-column">
+                <ArtworkImage image={testImg1} />
+                <AboutTheWork description={"Description"} provenance={"Provenance"} />
+            </div>
+            <div className="lot__right-column">
+                
+            </div>
+        </div>
     )
 };
 
