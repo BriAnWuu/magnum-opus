341fdbe3cc858f3e192d8093a537b800553fe03a
brianwu <fslikeyou1033@yahoo.com>
2024-12-08 01:29:32 -0500

    add link

--- src/components/FeatureGallery/FeatureGallery.jsx
+++ src/components/FeatureGallery/FeatureGallery.jsx
@@ -1,7 +1,10 @@
+import { motion } from "framer-motion";
+import { useNavigate } from "react-router-dom";
 import { convertBC } from "../../utils/utils";
 import "./FeatureGallery.scss";
 
 function FeatureGallery({ index, artworks }) {
+    const navigate = useNavigate();
     return (
         <section className="artwork__feature-gallery">
             <h2>Featuring Gallery {+index + 1}</h2>
@@ -9,10 +12,20 @@ function FeatureGallery({ index, artworks }) {
                 <div className="artwork__item-container">
                     { artworks.map((artwork, idx) => 
                         <article key={idx} className="artwork__item">
-                            <img
+                            <motion.img
                                 className="artwork__item-image"
                                 src={ `${import.meta.env.VITE_APP_IMG_BASE_URL}/${artwork.id}.jpg` } 
                                 alt={ artwork.alt_text }
+                                initial={{ y: 0 }}
+                                whileHover={{ 
+                                    y: -20,
+                                    transition: {
+                                        type: "spring",
+                                        damping: 10,
+                                    } 
+                                }}
+                                exit={{ y: 0 }}
+                                onClick={() => navigate(`/artwork/${artwork.id}`)}
                             />
                             <h3 className="artwork__item-artist">
                                 { artwork.artist_title || "Unknown Artist" }
