b61d92838b69e5a2b8558d270d8d1d3893f1c9ad
brianwu <fslikeyou1033@yahoo.com>
2024-12-09 18:00:33 -0500

    add user profiles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,28 @@
+import { useEffect, useState } from "react";
 import Hero from "../../components/Hero/Hero";
 import Main from "../../components/Main/Main";
+import WhosWatching from "../../components/WhosWatching/WhosWatching";
 import "./Home.scss";
 
-function Home(prop) {
+function Home() {
+    const [isProfileSelected, setIsProfileSelected] = useState(false)
+    
+    useEffect(() => {
+        const user_id = sessionStorage.getItem("user_id");
+        if (user_id) {
+            setIsProfileSelected(true);
+        }
+    }, [])
+
+
     return (
-        <>
+        <div className="home">
+            { !isProfileSelected && 
+                <WhosWatching setIsProfileSelected={setIsProfileSelected} />
+            }
             <Hero />
             <Main />
-        </>
+        </div>
     )
 };
 
