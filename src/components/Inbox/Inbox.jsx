f09d0c99cf3f4bdbb5cc1f42016a6c8e2f82f506
brianwu <fslikeyou1033@yahoo.com>
2024-12-24 12:46:43 -0500

    add inbox message stylings

--- src/components/Inbox/Inbox.jsx
+++ src/components/Inbox/Inbox.jsx
@@ -2,9 +2,12 @@ import "./Inbox.scss";
 
 function Inbox({ children }) {
     return (
-        <ul className="broadcast__inbox">
-            { children }
-        </ul>
+        <section className="broadcast__inbox-container">
+            {/* <h4 className="broadcast__inbox-title">Don't Miss Out</h4> */}
+            <ul className="broadcast__inbox">
+                { children }
+            </ul>
+        </section>
     )
 };
 
